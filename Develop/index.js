424fadd5d529cad0bd1633df0f55c97366178378
joshuasamples2 <joshua.samples2@gmail.com>
2022-01-16 19:24:54 -0600

    finished project

---
+++ Develop/index.js
@@ -0,0 +1,106 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown =require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the projects title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Give a brief description of your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "Installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "instructions",
+    },
+    {
+        type: "input",
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+        name: "collaborators",
+    },
+    {
+        type: "rawlist",
+        message: "Choose a license ",
+        name: "license",
+        choices: [
+            "ISC",
+            "MIT",
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your github?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
+]
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        if (err){
+            console.error(err)
+        } else console.log('sucsess')
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', data)
+        })
+}
+
+// Function call to initialize app
+init();
+
+
+
+// .then(answers => {
+//     fs.writeFile('READMEexample.md',
+//     `[![License: ICL]
+// (https://img.shields.io/badge/License-${answers.license}-blue.svg)
+// ](https://opensource.org/licenses/${answers.license})
+// # ${answers.title}\n 
+// ## Project Description
+// ${answers.description}\n
+// ## Table of contents
+// * [Installation](#installation)
+// * [instructions](#instructions)
+// * [collaborators](#collaborators)
+// * [Contact and GitHub Info](questions)
+// ## Installation
+// ${answers.Installation}\n
+// ## instructions
+// ${answers.instructions}\n
+// ## collaborators
+// ${answers.collaborators}\n
+// ## Questions: \n
+// Github Profile: https://github.com/${answers.github} \n 
+// email: ${answers.email}\n
+// Screenshot of deployed application:\n
+// ![This is an image of the deployed application](${answers.image})`
+// ,
+//         (err) => err ? console.error(err) : console.log("ReadMe Created!"))
+// })
+
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  # ${data.title}
+  // ## Project Description
+  // ${data.description}
+  // ## Table of contents
+  // * [Installation](#installation)
+  // * [instructions](#instructions)
+  // * [collaborators](#collaborators)
+  // * [Contact and GitHub Info]
+  // ## Installation
+  // ${data.Installation}
+  // ## instructions
+  // ${data.instructions}
+  // ## collaborators
+  // ${data.collaborators}
+  // Github Profile: https://github.com/${data.github} 
+  // email: ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
